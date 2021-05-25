85fc4cc64a6c983dc806fb6ec836d805a969e294
goattyie@gmail.com <goattyie@gmail.com>
2021-05-26 00:54:45 +0300

    Отчеты html

---
+++ 2.2/DB/KursovayaFramework/Kursovaya/HtmlReport.cs
@@ -0,0 +1,48 @@
+﻿using Npgsql;
+using System.IO;
+
+namespace Kursovaya
+{
+    class HtmlReport
+    {
+        static string Folder = "html/";
+        string FileName { get; set; }
+        string[] Columns { get; set; }
+        public HtmlReport(string filename, string[] columns)
+        {
+            FileName = filename;
+            Columns = columns;
+        }
+        public void Parse(string query)
+        {
+            using(StreamWriter sw = new StreamWriter(Folder + FileName))
+            {
+                sw.Write("<style>table{margin: auto;}</style><table bordercolor=\"Black\"><tr>");
+                foreach (string colname in Columns)
+                    sw.Write($"<td>{colname}</td>");
+                sw.Write("</tr>");
+                using (NpgsqlConnection connect = SQL.GetConnection())
+                {
+                    try
+                    {
+                        connect.Open();
+                        NpgsqlCommand command = new NpgsqlCommand(query, connect);
+                        NpgsqlDataReader reader = command.ExecuteReader();
+                        int i = Columns.Length;
+                        while (reader.Read())
+                        {
+                            sw.Write("<tr>");
+                            for(int index = 0; index < Columns.Length; index++)
+                                sw.Write($"<td>{reader.GetValue(index)}</td>");
+                            sw.Write("</tr>"); 
+                        }
+                        sw.Write("</tr>");
+                        connect.Close();
+                    }
+                    catch { }
+                }
+                sw.Write("</table>");
+            }
+        }
+    }
+}
