4a361fca87389ef769756352215e8a247b8bd652
Goattyie <goattyie@gmail.com>
2021-11-05 16:15:46 +0300

    2

---
+++ 3.1/ОКС/4Lab/4Lab/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValueConverter.DemicalToBinary("24");
+            ValueConverter.DemicalToBinary("49");
+        }
+    }
+}
---
+++ 3.1/ОКС/4Lab/4Lab/ValueConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4Lab
+{
+    static class ValueConverter
+    {
+        public static string DemicalToBinary(string value)
+        {
+            Console.WriteLine($"Переводим {value} в двоичную сс: ");
+            var binary = string.Empty;
+            var number = int.Parse(value);
+            Console.WriteLine($"{value} в двоичную сс: ");
+
+            while(number > 1)
+            {
+                //Console.Write($"делим {number} на 2: ");
+                binary = binary.Insert(0, (number % 2).ToString());
+                number = number / 2;
+                    
+                //Console.WriteLine($"резульат {number}, остаток {binary[0]}");
+            }
+            //Console.WriteLine($"результат деления: {number}. Записываем в начало и получим: ");
+            binary = binary.Insert(0, number.ToString());
+            Console.WriteLine(binary);
+            return binary;    
+        } 
+        public static int BinaryToDemical(string numb)
+        {
+            Console.WriteLine($"Переводим {numb} в десятичную сс: ");
+            int result = 0;
+            for(int i = 0; i < numb.Length; i++)
+            {
+                var res = (int)(int.Parse(numb[i].ToString()) * (int)Math.Pow(2, 7 - i));
+                Console.WriteLine($"{numb[i]} * 2^{7-i} = {res}");
+                Console.WriteLine($"Прибавляем к результату: {result} + {res} = {result + res}");
+                result += res;
+            }
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
